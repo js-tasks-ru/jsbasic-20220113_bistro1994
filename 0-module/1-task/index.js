94577bf624259d8889e9fc40f04296e820731ac1
WolffRick <97842227+WolffRick@users.noreply.github.com>
2022-01-17 15:45:25 +0300

    Update index.js

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,14 +1,7 @@
 function sum(m, n) {
-<<<<<<< HEAD
       return m + n;
-=======
-  unction sum(m, n) {
-      return m + n;
-}
+  }
 let result = sum(2, 5);
 alert(result);
->>>>>>> d198ee6bb31bae7fedc4c42e2a26bfaca75ca8fb
-}
-let result = sum(2, 5);
-alert(result);
+
 
