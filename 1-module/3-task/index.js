0eac924040c47a4d0f2bad5c493e2b32ead270eb
WolffRick <bistro1994@mail.ru>
2022-01-20 00:09:26 +0300

    Решил третью задачу

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str == '') return str.charAt();
+  else if (str.lenght == 1) return str[0].toUpperCase();
+    else {
+  let nstr = str[0].toUpperCase() + str.slice(1);
+    return nstr;
+    }
 }
