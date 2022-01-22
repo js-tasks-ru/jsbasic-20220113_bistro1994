f9ba3a79c83b72275aed23039a71577fd7699a3e
WolffRick <bistro1994@mail.ru>
2022-01-22 17:46:26 +0300

    внес необходимые правки

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,7 +1,6 @@
 function ucFirst(str) {
   if (str == '') return str.charAt();
-  else if (str.lenght == 1) return str[0].toUpperCase();
-    else {
+  else {
   let nstr = str[0].toUpperCase() + str.slice(1);
     return nstr;
     }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,5 +1,7 @@
 function truncate(str, maxlength) {
-  if (str.length > maxlength) 
+  if (str.length > maxlength) {
   return  str.slice(0, maxlength - 1) + '…';
-  else return str;
+  } else {
+  return str;
+  }
 }
